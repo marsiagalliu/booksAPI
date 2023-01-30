5cf52d6e8665fe00b2cf9816753ef7179bcbfb9f
Marsi <agalliumars@gmail.com>
2023-01-30 17:31:25 +0100

    Initial commit

---
+++ src/main/java/com/dojo/booksapi/repositories/BookRepository.java
@@ -0,0 +1,20 @@
+package com.dojo.booksapi.repositories;
+
+import com.dojo.booksapi.models.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, Long> {
+
+    List<Book> findAll();
+
+    List<Book> findByDescriptionContaining(String search);
+
+    Long countByTitleContaining(String search);
+
+    Long deleteByTitleStartingWith(String search);
+
+}
